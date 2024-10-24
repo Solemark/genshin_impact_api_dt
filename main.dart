a43c9eaed5948ccb60245869c7aec7f96531cde2
Solemark <larco9mason@gmail.com>
2024-10-24 17:40:57 +1000

    format and include doc comments

--- main.dart
+++ main.dart
@@ -4,17 +4,31 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart' as shelf_router;
 
-final _router = shelf_router.Router()
-  ..get("/", _generalHandler)
-  ..get("/<type>", _typeHandler)
-  ..get("/<type>/<name>", _nameHandler);
-
 final _jsonHeaders = {
   'content-type': 'application/json',
   'Cache-Control': 'no-store',
 };
 
+Future<void> main() async {
+  final int port = 8080;
+  final _router = shelf_router.Router()
+    ..get("/", _generalHandler)
+    ..get("/<type>", _typeHandler)
+    ..get("/<type>/<name>", _nameHandler);
+  final cascade = Cascade().add(_router.call);
+
+  final server = await shelf_io.serve(
+    logRequests().addHandler(cascade.handler),
+    InternetAddress.anyIPv4,
+    port,
+  );
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+/// Takes [Object]s and returns JSON String
 String _jsonEncode(Object data) => JsonEncoder.withIndent(' ').convert(data);
+
+/// returns JSON response for top level routes
 Response _generalHandler(Request request) {
   return Response(
     200,
@@ -27,6 +41,13 @@ Response _generalHandler(Request request) {
   );
 }
 
+/// returns JSON response for second level routes
+Future<Response> _typeHandler(Request request, String type) async {
+  Object res = await _getListData(type);
+  return Response(200, headers: _jsonHeaders, body: _jsonEncode(res));
+}
+
+/// returns the content of a directory or "invalid directory" on [PathNotFoundException]
 Future<Object> _getListData(String type) async {
   try {
     List<String> res = await Directory("data/$type")
@@ -35,35 +56,21 @@ Future<Object> _getListData(String type) async {
         .toList();
     res.sort();
     return res;
-  } catch (e) {
+  } on PathNotFoundException {
     return _jsonEncode("directory: $type not found");
   }
 }
 
-Future<Response> _typeHandler(Request request, String type) async {
-  Object res = await _getListData(type);
-  return Response(200, headers: _jsonHeaders, body: _jsonEncode(res));
+/// returns JSON response for third level routes
+Future<Response> _nameHandler(Request request, String type, String name) async {
+  return Response(200, headers: _jsonHeaders, body: await _getData(type, name));
 }
 
+/// returns the content of a file or "invalid path" on [PathNotFoundException]
 Future<String> _getData(String type, String name) async {
   try {
     return await File("data/$type/$name.json").readAsString();
   } on PathNotFoundException {
     return _jsonEncode("path: $type/$name not found");
   }
-}
-
-Future<Response> _nameHandler(Request request, String type, String name) async {
-  return Response(200, headers: _jsonHeaders, body: await _getData(type, name));
-}
-
-Future<void> main() async {
-  final int port = 8080;
-  final cascade = Cascade().add(_router.call);
-  final server = await shelf_io.serve(
-    logRequests().addHandler(cascade.handler),
-    InternetAddress.anyIPv4,
-    port,
-  );
-  print('Serving at http://${server.address.host}:${server.port}');
 }
